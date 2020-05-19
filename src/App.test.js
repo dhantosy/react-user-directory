b0d7bb619ad32782fbe16f04fa4dea244a566200
dhantosy <dhantosy@gmail.com>
2020-05-19 16:36:24 +0700

    added several unit test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import HeaderActions from './components/Header/HeaderActions';
+import UserCard from './components/UserCard/UserCard';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  const app = shallow(<App />);
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('initializes the `state` with null selected color filter', () => {
+    expect(app.state().isColorSelected).toEqual(null)
+  });
+
+  it('initializes the `state` with null selected city filter', () => {
+    expect(app.state().isCitiesSelected).toEqual(null)
+  });
+});
+
+
+describe('HeaderActions', () => {
+  const initialHeaderActionsProps = {
+    filterColorState: jest.fn(),
+    filterCitiesState: jest.fn()
+  }
+
+  const headerActions = shallow(<HeaderActions {...initialHeaderActionsProps} />);
+
+  it('adds boolean to selected color filter `state` when clicked', () => {
+    headerActions.find('#btnFilterColor').simulate('click');
+
+    expect(headerActions.state().btnColorState).toEqual(true || false);
+  });
+
+  it('adds boolean to selected city filter `state` when clicked', () => {
+    headerActions.find('#btnFilterCity').simulate('click');
+
+    expect(headerActions.state().btnCitiesState).toEqual(true || false);
+  });
 });
